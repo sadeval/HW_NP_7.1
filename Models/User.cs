935a962b145c10b679fcbe2b29a7fff9c93de634
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-27 19:48:22 +0200

    User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -2,9 +2,9 @@
 {
     public class User
     {
-        public int Id { get; set; } // Идентификатор пользователя
+        public int Id { get; set; } 
         public string Username { get; set; }
-        public string Password { get; set; } // В реальном приложении пароль должен быть захеширован
+        public string Password { get; set; } 
         public string Email { get; set; }
     }
 }
